1d3d840cb7cd787c21c803939b47ede1fe27e31a
drmats <drmats@users.noreply.github.com>
2014-12-19 15:35:29 +0100

    ctrl+mouse_scroll adjust camera fovy.

--- src/main_loop.cpp
+++ src/main_loop.cpp
@@ -73,8 +73,14 @@ void MainLoop::default_handler_t::mouse_wheel (
     const Uint8 *keystate = SDL_GetKeyboardState(NULL);
 
     if (keystate[SDL_SCANCODE_LCTRL] == 1) {
-        // ...
+        // set fovy
+        ml->camera.projection.set_all([ml, e] () {
+            auto all = ml->camera.projection.get_all();
+            all[0] += e.wheel.y*4;
+            return all;
+        }());
     } else {
+        // set camera distance-to-target
         ml->camera.dist =
             10.0f + std::exp2(std::fabs(
                 std::log2(ml->camera.dist - 10.0f) - e.wheel.y*0.2f
@@ -148,7 +154,14 @@ void MainLoop::default_handler_t::keyboard (
             break;
         case SDLK_c:
             if (e.key.state == SDL_PRESSED) {
+                // reset position
                 ml->camera.translation.load_identity();
+                // reset camera.projection.fovy
+                ml->camera.projection.set_all([ml] () {
+                    auto all = ml->camera.projection.get_all();
+                    all[0] = 60.0f;
+                    return all;
+                }());
             }
         default:
             break;
