d3beb0ccfdcacd17d7232ff4c813a7c1f9277059
drmats <drmats@users.noreply.github.com>
2015-01-06 17:37:46 +0100

    Added GFrame::get_matrix function.

--- src/gframe.cpp
+++ src/gframe.cpp
@@ -22,9 +22,42 @@ namespace machina {
  */
 template <typename T>
 GFrame<T>::GFrame ():
-    forward { static_cast<T>(0.0f), static_cast<T>(0.0f), static_cast<T>(-1.0f) },
-    up { static_cast<T>(0.0f), static_cast<T>(1.0f), static_cast<T>(0.0f) }
+    forward { static_cast<T>(0), static_cast<T>(0), static_cast<T>(-1) },
+    up { static_cast<T>(0), static_cast<T>(1), static_cast<T>(0) }
     {}
+
+
+
+
+/**
+ *  Assemble the transformation matrix.
+ */
+template <typename T>
+m3d::GMatrix4<T> GFrame<T>::get_matrix () const {
+    mat4 m;
+    vec3 right; right.cross(this->up, this->forward);
+
+    #define set(i, val) m[i] = val
+    #define up this->up
+    #define forward this->forward
+    #define origin this->origin
+    #define zero static_cast<T>(0)
+    #define one static_cast<T>(1)
+
+    set(0, right[0]);  set(4, up[0]);  set( 8, forward[0]);  set(12, origin[0]);
+    set(1, right[1]);  set(5, up[1]);  set( 9, forward[1]);  set(13, origin[1]);
+    set(2, right[2]);  set(6, up[2]);  set(10, forward[2]);  set(14, origin[2]);
+    set(3,     zero);  set(7,  zero);  set(11,       zero);  set(15,       one);
+
+    #undef one
+    #undef zero
+    #undef origin
+    #undef forward
+    #undef up
+    #undef set
+
+    return m;
+}
 
 
 
--- src/gframe.hpp
+++ src/gframe.hpp
@@ -26,14 +26,14 @@ class GFrame {
 
 public:
 
-    using Matrix = m3d::GMatrix4<T>;
-    using Vector3 = m3d::GVector3<T>;
+    using vec3 = m3d::GVector3<T>;
+    using mat4 = m3d::GMatrix4<T>;
 
 
     /**
      *  Vectors defining this frame.
      */
-    Vector3
+    vec3
         origin,
         forward,
         up;
@@ -46,7 +46,7 @@ public:
 
 
     /**
-     *  Translate this frame of reference in a world coordinates.
+     *  Translate this frame of reference in the world coordinates.
      */
     inline void translate_x (T delta) { this->origin[0] += delta; }
     inline void translate_y (T delta) { this->origin[1] += delta; }
@@ -56,24 +56,28 @@ public:
         this->translate_y(delta_y);
         this->translate_z(delta_z);
     }
-    inline void translate_world (const Vector3 &v) {
+    inline void translate_world (const vec3 &v) {
         this->origin.add(v);
     }
 
 
     /**
-     *  Translate this frame of reference in a local coordinates.
+     *  Translate this frame of reference in the local coordinates.
      */
     inline void translate_right (T delta) {
-        Vector3 right; right.cross(this->up, this->forward);
-        this->origin.add(right.scale(delta));
+        vec3 right;
+        this->origin.add(
+            right
+                .cross(this->up, this->forward)
+                .scale(delta)
+        );
     }
     inline void translate_up (T delta) {
-        Vector3 up(this->up);
+        vec3 up(this->up);
         this->origin.add(up.scale(delta));
     }
     inline void translate_forward (T delta) {
-        Vector3 forward(this->forward);
+        vec3 forward(this->forward);
         this->origin.add(forward.scale(delta));
     }
     inline void translate_local (T delta_x, T delta_y, T delta_z) {
@@ -81,11 +85,17 @@ public:
         this->translate_up(delta_y);
         this->translate_forward(delta_z);
     }
-    inline void translate_local (const Vector3 &v) {
+    inline void translate_local (const vec3 &v) {
         this->translate_right(v[0]);
         this->translate_up(v[1]);
         this->translate_forward(v[2]);
     }
+
+
+    /**
+     *  Assemble the transformation matrix.
+     */
+     m3d::GMatrix4<T> get_matrix () const;
 
 };
 
