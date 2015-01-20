3259b21592919c60aa44942f1c3d1bc2fe234052
drmats <drmats@users.noreply.github.com>
2015-01-20 18:44:08 +0100

    Test mesh added.

---
+++ src/test_mesh.hpp
@@ -0,0 +1,34 @@
+/**
+ *  machina
+ *
+ *  Copyright (c) 2014, drmats
+ *  All rights reserved.
+ *
+ *  https://github.com/drmats/machina
+ */
+
+#ifndef __TEST_MESH_HPP_
+#define __TEST_MESH_HPP_ 1
+
+#include "batch.hpp"
+#include <vector>
+#include <memory>
+
+namespace machina {
+    namespace primitives {
+
+
+
+
+/**
+ *  Something built with triangles...
+ */
+std::shared_ptr<TriangleBatch> test_mesh ();
+
+
+
+
+    } // namespace primitives
+} // namespace grid
+
+#endif
